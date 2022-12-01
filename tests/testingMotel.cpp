a1c018a6540ab266d9339a04464a76df8f937568
lindgh <lindaghun108@gmail.com>
2022-12-01 13:44:09 -0800

    added motel testing from ahan058

--- tests/testingMotel.cpp
+++ tests/testingMotel.cpp
@@ -1,86 +1,88 @@
-// #include <gtest/gtest.h>
-// #include "gmock/gmock.h"
-// #include "../header/Motel.hpp"
-// #include "../header/Detective.hpp"
-// using testing::InSequence;
-// using namespace std;
+#include <gtest/gtest.h>
+#include "gmock/gmock.h"
+#include "../header/Motel.hpp"
+#include "../header/Detective.hpp"
+using testing::InSequence;
+using ::testing::NiceMock;
+using namespace std;
 
-// //stub for playScene
-// class StubMotel: public Motel{
-//     int logic[1] = {1};
-//     int index = 0;
-
-//     public:
-//     int getNext(){
-//         if (index == 0){
-//             return 0;
-//         }
-//         int choice = logic[index];
-//         index++;
-//         return choice;
-//     }
-// };
+class FakeMotel {
+    public:
+    virtual void goToMyRoom() = 0;
+    virtual void exitRoom203AfterExplore() = 0;
+    virtual void exploreBathroom() = 0;
+    virtual void exploreDrawers() = 0;
+    virtual void exploreNightStand() = 0;
+};
 
 
-// class MockMotel: public Motel{
-//     public:
-//     MOCK_METHOD(int, getNext,(), ());
+class MockMotel: public FakeMotel {
+    public:
+    MOCK_METHOD(void,goToMyRoom,(),(override));
+    MOCK_METHOD(void, exitRoom203AfterExplore,(),(override));
+    MOCK_METHOD(void, exploreBathroom,(),(override));
+    MOCK_METHOD(void, exploreDrawers,(),(override));
+    MOCK_METHOD(void,exploreNightStand,(),(override));
 
-//     void DelegateToFake(){
-//         ON_CALL(*this, getNext).WillByDefault([this] (){
-//             return fake_.getNext();
-//         });
-//     }
-
-//     private:
-
-//         StubMotel fake_;
+};
 
 
-// }; 
+void exploreRoom(int choice, FakeMotel &m){
+    switch (choice){
+        case 1: m.exploreBathroom();
+        break;
+        case 2: m.exploreDrawers();
+        break;
+        case 3: m.exploreNightStand();
+        break;
+    }
+}
+
+void playScene(int choice,FakeMotel &m){
+
+    switch(choice){
+        case 1: exploreRoom(choice, m);
+        break;
+        case 2: m.goToMyRoom();
+    }
+}
 
 
-// class MockMotelActions: public Motel{
-//     public:
-    
-//     MOCK_METHOD(void, exploreRoom,(Detective *d), ());
-//     MOCK_METHOD(void,goToMyRoom,(Detective *d),());
-//     MOCK_METHOD(void,exploreBathroom,(Detective *d),());
-//     MOCK_METHOD(void,exploreDrawers,(Detective *d),());
-//     MOCK_METHOD(void,exploreNightStand,(Detective *d),());
-//     MOCK_METHOD(void,exitRoom203AfterExplore,(Detective *d),());
-//     MOCK_METHOD(void,playScene,(Detective *d),(override));
-//     MOCK_METHOD(void, validateInput,(int &t, int x, int y),());
-// };
-
-// TEST(MotelTests, testPayScene) {
-//     Detective *d = new Detective();
-//     StubMotel sm;
-//     MockMotelActions mockLogic;
-
-//     Motel m;
-
-//     // m.playScene(d);
-//     int forcedChoice = 1;
-//     sm.validateInput(forcedChoice, 4,1);
-//     EXPECT_CALL(mockLogic, exploreRoom(d));
 
 
-//     // {
-//     //     InSequence sequence;
-
-//     //     int choice = sm.getNext();
-//     //     int forcedChoice = 1;
-//     //     sm.validateInput(forcedChoice, 4,1);
-//     //     EXPECT_CALL(mockLogic, exploreRoom(d));
-//     //     // EXPECT_CALL(mockAwards, awardBronze(mrl.getNext()));
-//     //     // EXPECT_CALL(mockAwards, awardSilver(mrl.getNext()));
-//     //     // EXPECT_CALL(mockAwards, awardGold(mrl.getNext()));
-//     //     // EXPECT_CALL(mockAwards, turnOffTheLightsAndGoHome());
+TEST(TestMotel, testLogicPlayScene){
+    NiceMock<MockMotel> mm;
+    //MockMotel mm;
+    playScene(2,mm);
+    ON_CALL(mm,goToMyRoom());
 
 
-//     // }
+}
 
-   
-//     delete d;
-// }
+TEST(TestMotel, testExploreRoomDrawers){
+    NiceMock<MockMotel> mm;
+    //MockMotel mm;
+    exploreRoom(2,mm);
+    ON_CALL(mm,exploreDrawers());
+
+
+}
+
+TEST(TestMotel, testExploreBathroom){
+    NiceMock<MockMotel> mm;
+    //MockMotel mm;
+    exploreRoom(2,mm);
+    ON_CALL(mm,exploreBathroom());
+
+
+}
+
+
+TEST(TestMotel, testExploreNightStand){
+    NiceMock<MockMotel> mm;
+    //MockMotel mm;
+    exploreRoom(2,mm);
+    ON_CALL(mm,exploreNightStand());
+
+
+}
