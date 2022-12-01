1ba30ef150cdd45fc161d667139ecb1a9ca73a98
AdlaiMB <adlaimb@gmail.com>
2022-12-01 12:58:37 -0800

    add testing to Mansons and CountryClub

---
+++ tests/testingCountryClub.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include "../header/CountryClub.hpp"
+
+TEST(CountryClubTests, firstTest)
+{
+    Detective *d = new Detective();
+    CountryClub location;
+    delete d;
+}
---
+++ tests/testingMansons.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include "../header/Mansons.hpp"
+
+using namespace std;
+
+TEST(MansonsTests, firstTest)
+{
+    Detective *d = new Detective();
+
+    Mansons location;
+    delete d;
+}
